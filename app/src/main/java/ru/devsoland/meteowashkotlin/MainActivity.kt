2aae927869da5c74c5e43e4f62811ba556146c6b
solandmedotru <solomin.andrey@gmail.com>
2018-09-30 22:34:30 +0300

    added simple bottom navigation with fragments

--- app/src/main/java/ru/devsoland/meteowashkotlin/MainActivity.kt
+++ app/src/main/java/ru/devsoland/meteowashkotlin/MainActivity.kt
@@ -2,11 +2,39 @@ package ru.devsoland.meteowashkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_weather -> {
+                val songsFragment = WeatherFragment.newInstance()
+                openFragment(songsFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation__map -> {
+                val albumsFragment = MapFragment.newInstance()
+                openFragment(albumsFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
---
+++ app/src/main/java/ru/devsoland/meteowashkotlin/MapFragment.kt
@@ -0,0 +1,17 @@
+package ru.devsoland.meteowashkotlin
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MapFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.map_fragment, container, false)
+
+    companion object {
+        fun newInstance(): MapFragment = MapFragment()
+    }
+}
